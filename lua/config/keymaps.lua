bb22b765b477c316c6c37734d490b1ee7bc8bf63
Ryan Arana <ryan.arana@dexcom.com>
2023-11-16 10:55:55 -0800

    Add a keymap for <Ctrl-space> to escape Terminal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Use Ctrl+Space to enter Normal mode from Terminal mode.
+vim.keymap.set("t", "<C-space>", "<C-\\><C-n><C-space>", { silent = true })
